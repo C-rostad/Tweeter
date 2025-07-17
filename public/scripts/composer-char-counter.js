18d55035ef37c2917b4e0bab7cfdfb5adfe7a484
C-rostad <Carter@rostad.ca>
2025-07-17 09:28:57 -0700

    implement character counter, added red color if it  turns negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  const chars = 140
+  $("textarea").on("input", function () {
+
+    const inputLength = $(this).val().length;
+    const remaining = chars - inputLength
+
+    const $counter = $(this).closest("form").find('[name="counter"]');
+    $counter.text(remaining);
+
+    // Add/remove the 'negative' class based on the value
+    if (remaining < 0) {
+      $counter.addClass("negative");
+    } else {
+      $counter.removeClass("negative");
+    }
+  })
+});
--- server/index.js
+++ server/index.js
@@ -31,6 +31,8 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/api/tweets", tweetsRoutes);
 
+
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
